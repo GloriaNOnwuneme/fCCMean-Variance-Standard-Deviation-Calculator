ec18bc3353e3ac55f7a772bc75a8de4a784c54eb
Gloria Onwuneme <gn.onwuneme@yahoo.com>
2024-07-03 13:35:36 +0000

    Added solution

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,20 @@
 import numpy as np
 
-def calculate(list):
+def calculate(arr):
+    if (len(arr) != 9) or not all(type(i) in [int, float] for i in arr):
+        raise ValueError('List must contain nine numbers.')
 
-
-
-
-    return calculations
+    matr = np.reshape(arr, newshape=(3,3))
+    
+    def oper(fn):
+        return [[fn(matr[:,i]) for i in range(3)],[fn(matr[i,:]) for i in range(3)], fn(arr)]
+    
+    
+    return {
+        'mean': oper(np.mean),
+        'variance': oper(np.var),
+        'standard deviation': oper(np.std),
+        'max': oper(np.max),
+        'min': oper(np.min),
+        'sum': oper(np.sum)
+    }
